d35a92640f0483796b7e3b142a8d6803d95df3da
aa6910 <aliyanamin1066@gmail.com>
2025-12-31 19:25:19 +0000

    Update Doublepend2.py

--- Doublepend2.py
+++ Doublepend2.py
@@ -1,59 +1,30 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 30 18:21:13 2025
-
-@author: deck
-"""
-
 import numpy as np
+from scipy.integrate import solve_ivp
+from scipy.integrate import odeint
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from vpython import *
-import sympy
 
 g = 9.81
 L1 = 1
 L2 = 1
 m1 = 1
 m2 = 1
-t = 0
-dt = 0.01
 
-theta1 = np.pi/4
-theta2 = np.pi/3
-w1 = 0
-w2 = 0
+def double_pendulum(t, z):
+    theta1, theta2, w1, w2 = z
+    dw1dt = (-g*(2*m1 + m2)*np.sin(theta1) - (m2)*g*np.sin(theta1 - 2*theta2) - 2*np.sin(theta1 - theta2)*(m2)*(L2*(w2)**2+ L1*np.cos(theta1 - theta2)*L1*(w1)**2))/(L1*(2*m1 + m2 - (m2)*np.cos(2*theta1 - 2*theta2)))
+    dw2dt = (2*np.sin(theta1 - theta2)*((L1)*(m1+m2)*w1**2)+g*(m1+m2)*np.cos(theta1) +(L2)*(m2)*np.cos(theta1-theta2)*(w2)**2)/((L2)*(2*m1 + m2 + (m2)*np.cos(2*theta1 - 2*theta2)))
+    return [w1, w2, dw1dt, dw2dt]
 
-bob_1 = gcurve(color = color.red)
-bob_2 = gcurve(color = color.blue)
-
-def acc1(theta1, theta2, w1, w2):
-    #return (-g*(2*m1 + m2)*np.sin(theta1) - (m2)*g*np.sin(theta1 - 2*theta2) - 2*np.sin(theta1 - theta2)*(m2)*(L2*(w2)**2+ L1*np.cos(theta1 - theta2)*L1*(w1)**2))/(L1*(2*m1 + m2 - (m2)*np.cos(2*theta1 - 2*theta2))+1e-10)
-    num1 = -g*(2*m1 + m2)*np.sin(theta1)
-    num2 = -m2*g*np.sin(theta1 - 2*theta2)
-    num3 = -2*np.sin(theta1 - theta2)*m2*(w2**2*L2 + w1**2*L1*np.cos(theta1 - theta2))
-    den = L1*(2*m1 + m2 - m2*np.cos(2*theta1 - 2*theta2))
-    alpha1 = (num1 + num2 + num3) / den
-    return alpha1
-def acc2(theta1, theta2, w1, w2):
-    #return (2*np.sin(theta1 - theta2)*((L1)*(m1+m2)*w1**2)+g*(m1+m2)*np.cos(theta1) +(L2)*(m2)*np.cos(theta1-theta2)*(w2)**2)/((L2)*(2*m1 + m2 + (m2)*np.cos(2*theta1 - 2*theta2))+1e-10)
-    num1 = 2*np.sin(theta1 - theta2)
-    num2 = (w1**2*L1*(m1+m2))
-    num3 = g*(m1+m2)*np.cos(theta1)
-    num4 = w2**2*L2*m2*np.cos(theta1 - theta2)
-    den = L2*(2*m1 + m2 - m2*np.cos(2*theta1 - 2*theta2))
-    alpha2 = (num1*(num2 + num3 + num4)) / den
-    return alpha2
-    
-while True:
-    rate(500)
-    theta1_new = theta1 + w1*dt + 0.5*acc1(theta1, theta2, w1, w2)*dt**2   
-    theta2_new = theta2 + w2*dt + 0.5*acc2(theta1, theta2, w1, w2)*dt**2 
-    w1_new = w1 + 0.5*dt*(acc1(theta1_new, theta2_new, w1, w2) + acc1(theta1, theta2, w1, w2))
-    w2_new = w2 + 0.5*dt*(acc2(theta1_new, theta2_new, w1, w2) + acc2(theta1, theta2, w1, w2))
-    theta1 = theta1_new
-    theta2 = theta2_new
-    t += dt
-    bob_1.plot(t,w1)
-    bob_2.plot(t,w2)
+T = [0, 100]
+initial = [1, 1, 0, 0]
+sol = solve_ivp(double_pendulum, T, initial, t_eval=np.linspace(0, 100, 1000))
+theta1 = sol.y[0]
+theta2 = sol.y[1]
+x1 = L1 * np.sin(theta1)
+y1 = -L1 * np.cos(theta1)
+x2 = x1 + L2 * np.sin(theta2)
+y2 = y1 - L2 * np.cos(theta2)
+plt.plot(sol.t, x1, label = '$\tx_{1}$')
+plt.plot(sol.t, x2, label = '$\tx_{2}$')
+plt.legend()
+plt.show()
